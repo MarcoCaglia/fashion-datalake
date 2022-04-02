bd9769ac365dcd7719da3f3f251be25040ad01e4
marco <cagliamarco@gmail.com>
2022-04-02 08:19:47 +0200

    Feature (zalando-spider): Add image extraction

--- src/fashion_datalake/scraping/scraping/spiders/zalando_spider.py
+++ src/fashion_datalake/scraping/scraping/spiders/zalando_spider.py
@@ -3,7 +3,7 @@ from hashlib import sha256
 import scrapy
 import yaml
 from fashion_datalake.scraping.scraping.items import ScrapingItem
-from fashion_datalake.utils.constants import SPIDER_CONFIG_PATH
+from fashion_datalake.utils.constants import SPIDER_CONFIG_PATH, ZALANDO_SOURCE_NAME
 from scrapy_splash import SplashRequest
 
 
@@ -13,6 +13,9 @@ class ZalandoSpiderSpider(scrapy.Spider):
         config = yaml.safe_load(f)
         allowed_domains = config[name].get("allowed_domains")
         start_urls = config[name].get("start_urls")
+
+    # Spider specific settings
+    SOURCE = ZALANDO_SOURCE_NAME
 
     def start_requests(self):
         """Request webpages through Splash API."""
@@ -31,22 +34,20 @@ class ZalandoSpiderSpider(scrapy.Spider):
 
         # Follow all links
         for link in item_links:
-            link = self.conform_request_schema(link)
+            link = self._conform_request_schema(link)
             yield SplashRequest(url=link, callback=self.extract_item)
 
         # After all links have been followed, go to the next page, if there is
         # one available
-        next_page = response.xpath(
-            "/html/body/div/div/div/div/div/div/div/nav/div/a/@href"
-        ).extract()
+        next_page = response.css("a.DJxzzA.PgtkyN::attr(href)").extract()
         next_page = (
             next_page[-1]
-            if len(next_page) > 1 or next_page[-1].endswith("p=2")
+            if len(next_page) > 1 or next_page[-1].find("p=2") > -1
             else None
         )
 
         if next_page is not None:
-            next_page = self.conform_request_schema(next_page)
+            next_page = self._conform_request_schema(next_page)
             yield SplashRequest(url=next_page, callback=self.parse)
 
     def extract_item(self, response):
@@ -57,19 +58,17 @@ class ZalandoSpiderSpider(scrapy.Spider):
         # Fill item information
         item["id_item"] = sha256(response.url.encode("utf-8")).hexdigest()
         item["url"] = response.url
-        item["source"] = "zalando"
+        item["source"] = self.SOURCE
         item["raw_html"] = response.text
 
         # Fill in image info TODO
-
-        yield item
-
-        # Load images
         item["image_urls"] = response.css(
             "img.RYghuO.u-6V88.ka2E9k.uMhVZi.FxZV-M._2Pvyxl.JT3_zV.EKabf7.mo6ZnF._1RurXL.mo6ZnF._7ZONEy"
         ).extract()
 
-    def conform_request_schema(self, url: str) -> str:
+        yield item
+
+    def _conform_request_schema(self, url: str) -> str:
         """Ensure that the passed URL complies with the required URL schema.
 
         Args:
