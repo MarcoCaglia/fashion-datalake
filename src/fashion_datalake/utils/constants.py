fbc7cd38d97e89b4555753c18790bcf5cfb556e9
marco <cagliamarco@gmail.com>
2022-04-02 08:20:02 +0200

    Feature (fashion-datalake): Add unittests

--- src/fashion_datalake/utils/constants.py
+++ src/fashion_datalake/utils/constants.py
@@ -4,3 +4,8 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parents[3].resolve()
 SPIDER_CONFIG_PATH = ROOT_DIR.joinpath("properties", "spider_config.yml")
+
+TEST_ASSETS_PATH = ROOT_DIR.joinpath("test", "test_assets")
+
+# Source names
+ZALANDO_SOURCE_NAME = "zalando"
